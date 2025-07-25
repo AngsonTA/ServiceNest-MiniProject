1009859de14f434c44d9150481d29228d2d7489b
AngsonTA <angsontheekan@gmail.com>
2025-07-25 22:18:13 +0530

    created store and carts funtionality

--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from category.models import Category
-
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 def store(request, category_slug=None):
     categories = None
     products = None
@@ -9,13 +12,18 @@ def store(request, category_slug=None):
     if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
-        product_count = products.count()
     else:
-        products = Product.objects.filter(is_available=True)
-        product_count = products.count()
+        products = Product.objects.filter(is_available=True).order_by('id')
+
+    # Apply pagination to all products (regardless of category)
+    paginator = Paginator(products, 6)  # show 6 products per page
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
+
+    product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
         'selected_category': categories,
     }
@@ -25,10 +33,30 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-    except Exception as e:
-        raise e
+        in_cart = CartItem.objects.filter(
+            cart__cart_id=_cart_id(request),
+            product=single_product
+        ).exists()  # True if item is in cart
+    except Product.DoesNotExist:
+        return redirect('store')  # Redirect if product not found
 
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products' : products,
+        'product_count' : product_count,
+    }
+    
+    return render(request, 'store/store.html', context)
